7cfc4e91ac49eb846fa20a8d64901073ae8d0a84
Kal Ponn <kalponnadagt@gmail.com>
2018-12-20 18:12:14 -0500

    homework3  submission
    homework 3 submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,15 +10,17 @@ csvpath = os.path.join("..", "Resources", "budget_data.csv")
 # Specify the file to write to
 outfile = '../Resources/PyBankoutput.txt'
 
+# Iniitialize variables
 totalmonths = 0
 totalamt = 0
 
+# open the csv file
 with open(csvpath, newline="") as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=",")
 	linecount = 0
 	
     
-# Loop through looking for the video
+# Loop through the file and  determine total amt , max increase , decrease .
 	for row in csvreader:
 		if linecount == 0:
 			currentamt = 0
@@ -35,8 +37,8 @@ with open(csvpath, newline="") as csvfile:
 			totalamt = totalamt + currentamt
 			curramtchange = currentamt - lastamt
 			if linecount == 1:
+				#store first row amt
 				firstrowamt = currentamt
-				
 			else:
 				if curramtchange > maxincreaseamt:
 					maxincreaseamt = curramtchange 
@@ -49,27 +51,28 @@ with open(csvpath, newline="") as csvfile:
 		lastamt = currentamt
 		linecount += 1
 
-
+	#store the last row amt
 	lastrowamt = currentamt
+
+	#calculate average change
 	averagechange = round((lastrowamt - firstrowamt ) / (totalmonths - 1),2)
 
-	print("total months" + str(totalmonths))
-	print("total amount" + str(totalamt))
-	print("average change" + str(averagechange))
-	print("maximum increase"+ str(maxincreaseamt))
-	print("maximum increase month"+ str(maxincreasemonth))
-	print("maximum decrease"+ str(maxdecreaseamt))
-	print("maximum decrease month"+ str(maxdecreasemonth))
+	#print the values to terminal
+	print("Total Months: " + str(totalmonths))
+	print("Total: " +"$"+ str(totalamt))
+	print("Average Change: " +"$"+ str(averagechange))
+	print("Greatest Increase in Profits: "+ str(maxincreasemonth) + " ($" + str(maxincreaseamt) + ")")
+	print("Greatest Decrease in Profits: "+ str(maxdecreasemonth) + " ($" + str(maxdecreaseamt) + ")")
+	
 
 
 	# Open the file using "write" mode. Specify the variable to hold the contents
 with open(outfile, 'w') as file:
-	file.write("total months" + str(totalmonths) + "\n")
-	file.write("total amount" + str(totalamt) + "\n")
-	file.write("average change" + str(averagechange) + "\n")
-	file.write("maximum increase"+ str(maxincreaseamt) + "\n")
-	file.write("maximum increase month"+ str(maxincreasemonth) + "\n")
-	file.write("maximum decrease"+ str(maxdecreaseamt) + "\n")
-	file.write("maximum decrease month"+ str(maxdecreasemonth) + "\n")
+	file.write("Total Months: "  + str(totalmonths) + "\n")
+	file.write("Total: "  +"$"+ str(totalamt) + "\n")
+	file.write("Average Change: " +"$"+ str(averagechange) + "\n")
+	file.write("Greatest Increase in Profits: "+ str(maxincreasemonth) + " ($" + str(maxincreaseamt) + ")\n")
+	file.write("Greatest Decrease in Profits: "+ str(maxdecreasemonth) + " ($" + str(maxdecreaseamt) + ")\n")
+		
 
 
