4c614ec6727b898c17824b14934473f5bd81dc2f
Kal Ponn <kalponnadagt@gmail.com>
2018-12-20 15:01:28 -0500

    wip
    wip

---
+++ PyPoll/main.py
@@ -0,0 +1,77 @@
+# Modules
+import os
+import csv
+
+
+# Set path for input file read
+csvpath = os.path.join("..", "Resources", "election_data.csv")
+
+
+# Specify the file to write to
+outfile = '../Resources/PyPolloutput.txt'
+
+# set the variables to store total votes, candidates list , votes for each candidates, and percentages
+totalvotes = 0
+candidates = []
+votesforcandidate = []
+votespercent =[]
+totalcandidatevotes = []
+
+# open the csv file and 
+with open(csvpath, newline="") as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=",")
+	
+	# Read the header row first
+	csv_header = next(csvfile)
+	
+    
+	# Loop through the file and count the total votes, build the candidates list , votes for each candidate 
+	for row in csvreader:
+		totalvotes += 1
+		if row[2] in candidates:
+			a = candidates.index(row[2])
+			votesforcandidate[a] += 1
+		else:
+			candidates.append(row[2])
+			votesforcandidate.append(1)
+				
+	# Loop thru the candidates to calculate the votes percentage and store in a list	
+	i = 0
+	for i in range(len(candidates)):
+		votespercentage = (votesforcandidate[i]/ totalvotes ) * 100
+		votespercent.append(round(votespercentage,3))
+	
+	#determine which candidate got the maximum votes percentage
+	maxvotesindex = votespercent.index(max(votespercent))
+	winner = candidates[maxvotesindex]
+
+	#zip all the lists to build  candidate details record
+	totalcandidatevotes = zip(candidates,votespercent,votesforcandidate)
+
+	#print the details
+	print("Election Results                                ")
+	print("------------------------------------------------")
+	print("Total Votes: " + str(totalvotes))
+	print("------------------------------------------------")
+	for values in totalcandidatevotes:
+		print(values[0] + " : " + str(values[1]) + "%" + " (" + str(values[2]) + ")")
+	print("------------------------------------------------")
+	print("winner: " + winner)
+	print("------------------------------------------------")
+
+
+
+	# Open the file using "write" mode. Specify the variable to hold the contents
+with open(outfile, 'w') as file:
+	
+	file.write("Election Results                                "+ "\n")
+	file.write("------------------------------------------------"+ "\n")
+	file.write("Total Votes: " + str(totalvotes)+ "\n")
+	file.write("------------------------------------------------"+ "\n")
+	for values in totalcandidatevotes:
+		file.write(values[0] + " : " + str(values[1]) + "%" + " (" + str(values[2]) + ")"+ "\n")
+	file.write("------------------------------------------------"+ "\n")
+	file.write("winner: " + winner + "\n")
+	file.write("------------------------------------------------"+ "\n")
+	
+
